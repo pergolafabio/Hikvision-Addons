0802e9f13a3cfb37901f431abe67186778b680cf
pergolafabio <43251136+pergolafabio@users.noreply.github.com>
2022-05-31 10:23:39 +0200

    Update run.sh

--- hikvision-v4l2loopback/run.sh
+++ hikvision-v4l2loopback/run.sh
@@ -22,7 +22,8 @@ echo -e "Check if v4l2loopback modulde is loaded..."
 lsmod
 sleep 1
 echo -e "Injecting RTSP stream into /dev/video0..."
-ffmpeg -i ${RTSP} -nostats -hide_banner -nostdin -s 704*480 -r 20 -vf format=yuv420p -f v4l2 /dev/video0 > /dev/null &
+#ffmpeg -i ${RTSP} -nostats -hide_banner -nostdin -s 704*480 -r 20 -vf format=yuv420p -f v4l2 /dev/video0 > /dev/null &
+ffmpeg -i ${RTSP} -nostats -hide_banner -nostdin -s 640*480 -r 20 -vf format=yuv420p -f v4l2 /dev/video0 > /dev/null &
 echo -e "Waiting 8 seconds before starting linphone (/dev/video0 must be processed first)..."
 sleep 8
 echo -e "Starting linphone..."
