301b07f5070d2cd4685ba15200552a02463883a1
Carlo Mion <mion00@gmail.com>
2023-02-25 17:25:55 +0100

    Add test for raising exception

--- hikvision-doorbell/tests/test_input.py
+++ hikvision-doorbell/tests/test_input.py
@@ -1,6 +1,10 @@
+import asyncio
+import sys
+from unittest.mock import AsyncMock
 import pytest
 from pytest_mock import MockerFixture
 from input import InputReader
+from sdk.utils import SDKError
 
 
 def test_unlock_command(mocker: MockerFixture):
@@ -47,3 +51,22 @@ def test_reboot_command(mocker: MockerFixture):
     mocked_registry.getByName.assert_called_once_with("doorbell")
     mocked_doorbell = mocked_registry.getByName('doorbell')
     mocked_doorbell.reboot_device.assert_called_once()
+
+
+def test_not_raise_exception(mocker: MockerFixture):
+    """If there is an SDKError, the program should catch it and not crash"""
+    mocked_registry = mocker.patch('doorbell.Registry', autospec=True)
+    # Create a fake stdin reader
+    stdin = mocker.patch('asyncio.StreamReader')
+    mocker.patch('input.connect_stdin', side_effect=AsyncMock(return_value=stdin))
+    # Configure the list of commands to feed the mocked stdin
+    commands = [AsyncMock(return_value=bytes("reboot doorbell", 'utf-8'))(), AsyncMock(return_value=bytes("", 'utf-8'))()]
+    mocker.patch.object(stdin, 'readline', side_effect=commands)
+    
+    # Configure doorbell to raise exception when reboot is called
+    mocked_doorbell = mocked_registry.getByName('doorbell')
+    mocked_doorbell.reboot_device.side_effect = SDKError(mocker.patch("ctypes.CDLL"), "")
+    
+    reader = InputReader(mocked_registry)
+
+    asyncio.run(reader.loop_forever())
