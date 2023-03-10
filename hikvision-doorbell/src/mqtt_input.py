ff65c1061aa6e26cbe9da8900ff21e6c5ec26da6
Carlo Mion <mion00@gmail.com>
2023-03-10 08:21:01 +0100

    feat(MQTTInput): add input text to interact with ISAPI endpoint

--- hikvision-doorbell/src/mqtt_input.py
+++ hikvision-doorbell/src/mqtt_input.py
@@ -1,8 +1,9 @@
 import json
+from typing import Any
 from config import AppConfig
 from doorbell import DeviceType, Doorbell, Registry
-from ha_mqtt_discoverable import Settings
-from ha_mqtt_discoverable.sensors import Button, ButtonInfo
+from ha_mqtt_discoverable import Settings, Discoverable
+from ha_mqtt_discoverable.sensors import Button, ButtonInfo, Text, TextInfo
 from home_assistant import sanitize_doorbell_name
 from loguru import logger
 from mqtt import extract_device_info
@@ -12,6 +13,8 @@ from sdk.utils import SDKError
 
 
 class MQTTInput():
+    _sensors: dict[Doorbell, dict[str, Discoverable[Any]]] = {}
+
     def __init__(self, config: AppConfig.MQTT, doorbells: Registry) -> None:
         logger.debug("Setting up MQTTInput")
 
@@ -21,6 +24,8 @@ class MQTTInput():
             password=config.password
         )
         for doorbell in doorbells.values():
+            self._sensors[doorbell] = {}
+
             doorbell_name = doorbell._config.name
             device = extract_device_info(doorbell)
 
@@ -67,6 +72,20 @@ class MQTTInput():
             answer_button = Button(settings, self._answer_call_callback, doorbell)
             answer_button.set_availability(True)
 
+            ###########
+            # ISAPI command input text
+            text_info = TextInfo(
+                name="ISAPI endpoint",
+                unique_id=f"{sanitized_doorbell_name}_isapi_endpoint",
+                device=device,
+                enabled_by_default=False,
+                entity_category="diagnostic",
+                object_id=f"{sanitized_doorbell_name}_isapi_endpoint")
+            settings = Settings(mqtt=mqtt_settings, entity=text_info, manual_availability=True)
+            isapi_input = Text(settings, self._isapi_input_callback, doorbell)
+            isapi_input.set_availability(True)
+            self._sensors[doorbell]['isapi_input'] = isapi_input
+
     def _reboot_callback(self, client, doorbell: Doorbell, message: MQTTMessage):
         logger.info("Received reboot command for doorbell: {}", doorbell._config.name)
         # Avoid crashing inside the callback, otherwise we lose the MQTT client
@@ -104,3 +123,24 @@ class MQTTInput():
             doorbell._call_isapi("PUT", url, json.dumps(requestBody))
         except SDKError as err:
             logger.error("Error while answering call: {}", err)
+
+    def _isapi_input_callback(self, client, doorbell: Doorbell, message: MQTTMessage):
+        logger.debug("Received input text for doorbell: {}", doorbell._config.name)
+
+        text_string = message.payload.decode('utf-8')
+        self._sensors[doorbell]['isapi_input'].set_text(text_string)
+        
+        # Decode the HTTP method and URL by splitting the input string
+        http_method, url = text_string.split()
+        request_body = ""
+
+        # Avoid crashing inside the callback, otherwise we lose the MQTT client
+        try:
+            response = doorbell._call_isapi(http_method, url, request_body)
+            attributes = {
+                "request": text_string,
+                "response": response
+            }
+            self._sensors[doorbell]['isapi_input'].set_attributes(attributes)
+        except SDKError as err:
+            logger.error("Error while invoking ISAPI endpoint: {}", err)
