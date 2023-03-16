724ad6434580accb6d0938fc58af3c47f0887913
Pergola Fabio <43251136+pergolafabio@users.noreply.github.com>
2023-03-16 15:10:03 +0100

    Delete hikvision-baresip directory

--- hikvision-baresip/run.sh
+++
@@ -1,6 +0,0 @@
-cp /config/baresip/accounts /usr/bin/accounts
-cp /config/baresip/config /usr/bin/config
-#cp /config/baresip/contacts /usr/bin/contacts
-echo -e "Starting baresip..."
-baresip -f /usr/bin/
-#tail -f /dev/null
