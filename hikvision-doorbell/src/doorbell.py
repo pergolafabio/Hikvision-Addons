55f8bbb71e5cc085f543a9597294bc947b898e19
pergolafabio <pergola.fabio@gmail.com>
2025-11-19 15:09:25 +0100

    Add Support for DS-K1T321MFWX

--- hikvision-doorbell/src/doorbell.py
+++ hikvision-doorbell/src/doorbell.py
@@ -25,7 +25,8 @@ class DeviceType(IntEnum):
     K1T673 = 10534
     K1T342 = 10538
     K1T6QT = 10541
-    K1T341 = 10542  
+    K1T341 = 10542
+    DS-K1T321 = 10549
     K1T670 = 10552
     HD = 31
     AccessControlTerminal = 861
