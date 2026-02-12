98713efeb25aab865ed196a7996cb3f20bc46e8c
pergolafabio <pergola.fabio@gmail.com>
2026-02-12 21:08:07 +0100

    add falback resolution and quality, and try max resolution first

--- hikvision-doorbell/src/doorbell.py
+++ hikvision-doorbell/src/doorbell.py
@@ -321,7 +321,8 @@ class Doorbell():
             # Step 2: Capture Logic
             priority_channels = [1, 101]
             param_combinations = [
-                (0xFF, 2, 2048*1024) # (2, 2, 1024*1024),  # D1 size, Medium quality   (0, 2, 1024*1024)  # CIF size
+                (0xFF, 2, 2*1024*1024), # Try Max resolution first with a 2MB buffer
+                (16, 2, 1024*1024)    # Fallback to 720p if Max fails
             ]
 
             
